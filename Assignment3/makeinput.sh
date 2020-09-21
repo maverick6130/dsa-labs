d6a8b11a957e777b15c725777e563df2fbb14520
maverick6130 <daror6130@gmail.com>
2020-09-21 08:49:11 +0530

    reset

--- Assignment3/makeinput.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 20 > input.txt
-echo 1000000 >> input.txt
-for i in {1..1000000}
-do
-	echo "U 51724158235371" >> input.txt
-done
--- Assignment4/geninput.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-touch infile.txt
-echo $(printf "%19891s" 'a') > infile.txt
-echo "1" >> infile.txt
-time ./a.out < infile.txt
-rm infile.txt
